852145ea3ac3b178f17c34797722c8645eba37e4
Fabricio Rojas <fabriciovmamanir@gmail.com>
2023-04-06 16:51:30 -0500

    Added map

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,3 +1,49 @@
 'use strict';
 
 // Declaring elements
+
+const mapTrackBtn = document.querySelector('#start-map-btn');
+const flyToBtn = document.querySelector('#fly-btn');
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZmFicmljaW8tdm1yIiwiYSI6ImNsZzVqYW1tejAzZ24zbHFpMGhkbnMxcGgifQ.ZgmrUeDKUeNrVMRtPltK1Q';
+
+// Main Function
+mapTrackBtn.addEventListener('click', () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(trackLocation, errorHandler, {enableHighAccuracy: true})
+        flyToBtn.style.visibility = 'visible';
+    } else {
+        alert('Your browser does not support geolocation');
+    }
+})
+
+flyToBtn.addEventListener('click', () => {
+    map.flyTo({
+        center: [long, lat],
+        essential: true
+    })
+})
+
+let map;
+let lat, long;
+function trackLocation(position) {
+    const {latitude, longitude} = position.coords;
+    
+    lat = latitude;
+    long = longitude;
+    
+    map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v12',
+        center: [long, lat],
+        zoom: 16,
+    });
+
+    const yourLocation = new mapboxgl.Marker({color: '#1DA1F2'})
+        .setLngLat([long, lat])
+        .addTo(map);
+}
+
+function errorHandler() {
+    alert('Unable to get location')
+}
